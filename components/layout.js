104bf0117f1a9f597ce880cd1f3bfe5a5ad29876
Rakibul Islam <mdrakibul373@gmail.com>
2023-03-18 13:53:23 +0600

    feedback testing

--- components/layout.js
+++ components/layout.js
@@ -6,9 +6,9 @@ export default function Layout({ children, page, prev_page }) {
 	return (
 		<>
 			{/* <Header prev_page={prev_page} /> */}
+			<Sidenav />
 			<ion-content>
 				{/* <div className="wrapper"> */}
-				<Sidenav />
 				<div className="content">
 					<div className="content_without_footer">
 						<main className={`viewport ${page}`}>
